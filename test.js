009b4e24fef2aa8dfdd7f09b656d11e55dd26baf
anieharmon <anieharmon@icloud.com>
2023-05-01 14:19:58 -0600

    added test code

--- test.js
+++ test.js
@@ -24,3 +24,36 @@ fs.writeFile('logo.svg', template, err => {
   // file written successfully
 
 });
+
+function promptUser() {
+    inquirer
+      .prompt([
+        // Text prompt
+        {
+          type: "input",
+          message:
+            "What text would you like you logo to display? (Enter up to three characters)",
+          name: "text",
+        },
+        // Text color prompt
+        {
+          type: "input",
+          message:
+            "Choose text color (Enter color keyword OR a hexadecimal number)",
+          name: "textColor",
+        },
+        // Shape choice prompt
+        {
+          type: "list",
+          message: "What shape would you like the logo to render?",
+          choices: ["Triangle", "Square", "Circle"],
+          name: "shape",
+        },
+        // Shape color prompt
+        {
+          type: "input",
+          message:
+            "Choose shapes color (Enter color keyword OR a hexadecimal number)",
+          name: "shapeBackgroundColor",
+        },
+      ])}
