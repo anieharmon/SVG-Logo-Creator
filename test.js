46a718f93824473626a32fe57b4e58394e20b8d0
anieharmon <anieharmon@icloud.com>
2023-05-01 14:33:13 -0600

    shapes stuff

--- test.js
+++ test.js
@@ -8,14 +8,40 @@ const { Triangle, Square, Circle } = require("./lib/shapes");
 
 
 
-fs.writeFile('logo.svg', template, err => {
-  if (err) {
-    console.error(err);
-  }
-  console.log("File written successfully")
-  // file written successfully
+function writeToFile(fileName, answers) {
+    // File starts as an empty string
+    let svgString = "";
+    // Sets width and height of logo container
+    svgString =
+      '<svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg">';
+    // <g> tag wraps <text> tag so that user font input layers on top of polygon -> not behind
+    svgString += "<g>";
+    // Takes user input for shape choice and inserts it into SVG file
+    svgString += `${answers.shape}`;
 
-});
+    let shapeChoice;
+    if (answers.shape === "Triangle") {
+      shapeChoice = new Triangle();
+      svgString += `<polygon points="150, 18 244, 182 56, 182" fill="${answers.shapeBackgroundColor}"/>`;
+    } else if (answers.shape === "Square") {
+      shapeChoice = new Square();
+      svgString += `<rect x="73" y="40" width="160" height="160" fill="${answers.shapeBackgroundColor}"/>`;
+    } else {
+      shapeChoice = new Circle();
+      svgString += `<circle cx="150" cy="115" r="80" fill="${answers.shapeBackgroundColor}"/>`;
+    }
+
+    svgString += `<text x="150" y="130" text-anchor="middle" font-size="40" fill="${answers.textColor}">${answers.text}</text>`;
+    // Closing </g> tag
+    svgString += "</g>";
+    // Closing </svg> tag
+    svgString += "</svg>";
+  
+    // Using file system module to generate svg file, takes in file name given in the promptUser function, the svg string, and a ternary operator which handles logging any errors, or a "Generated logo.svg" message to the console  
+    fs.writeFile(fileName, svgString, (err) => {
+      err ? console.log(err) : console.log("Generated logo.svg");
+    });
+  }
 
 function promptUser() {
     inquirer
@@ -60,3 +86,5 @@ function promptUser() {
         }
       });
   }
+
+  promptUser();
