58a9860728976c0197f390ec4d4731da4bfecab5
anieharmon <anieharmon@icloud.com>
2023-05-01 14:22:33 -0600

    more index

--- test.js
+++ test.js
@@ -1,3 +1,13 @@
+const inquirer = require("inquirer");
+
+// File system module (node package manager) import
+const fs = require("fs");
+
+// Importing classes from ./lib/shapes directory
+const { Triangle, Square, Circle } = require("./lib/shapes");
+
+
+
 var classObject = {
     color: "pink",
     //shape: {
